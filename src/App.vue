e02225dd8cebd3f886ddbe550a426138b40d5895
yousefshanti <yousefshanti22@gmail.com>
2024-08-09 02:22:44 +0300

    end

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,26 @@
         <button @click="toggleSidebar" class="size-10 m-4 absolute">
           <img
             class="border-b-2 border-gray-200 object-cover"
-            src="path/to/icon1.png"
+            src="/Users/yousefshanti/Desktop/Screenshot 2024-07-30 at 1.46.58 PM.png"
             alt=""
           />
         </button>
         <div class="absolute top-14 border-b-2 left-4 border-gray-200">
-          <Icon class="size-7 mx-0 my-4" icon-code="Home"></Icon>
-          <Icon class="size-7 mx-0 my-4" icon-code="Settings"></Icon>
-          <img
-            class="size-7 mx-0 my-4"
-            src="path/to/icon2.png"
-            alt=""
-          />
-          <img
-            class="size-7 mx-0 my-4"
-            src="path/to/icon3.png"
-            alt=""
-          />
-          <!-- Add more icon wrappers or images as needed -->
+          <Icon class="mx-0 my-4" icon-code="icon1" />
+          <Icon class="mx-0 my-4" icon-code="icon2" />
+          <Icon class="mx-0 my-4" icon-code="icon3" />
+          <Icon class="mx-0 my-4" icon-code="icon4" />
+          <Icon class="mx-0 my-4" icon-code="icon5" />
+          <Icon class="mx-0 my-4" icon-code="icon6" />
+          <Icon class="mx-0 my-4" icon-code="icon7" />
+          <Icon class="mx-0 my-4" icon-code="icon8" />
         </div>
 
         <div class="left-2 absolute bottom-2">
-          <span class="p-3 text-xl rounded-full bg-stone-200 text-blue-500">FR</span>
-          <img
-            class="size-7 m-2 my-6"
-            src="path/to/icon4.png"
-            alt=""
-          />
+          <span class="p-3 text-xl rounded-full bg-stone-200 text-blue-500"
+            >FR</span
+          >
+          <Icon class="size-7 mx-3 my-6" icon-code="icon10" />
         </div>
       </div>
 
@@ -40,49 +33,103 @@
           <button @click="toggleSidebar" class="flex justify-start size-5/6">
             <img
               class="inline h-12 w-10"
-              src="path/to/icon1.png"
+              src="/Users/yousefshanti/Desktop/Screenshot 2024-07-30 at 1.46.58 PM.png"
               alt=""
             />
-            <span class="not-italic text-left justify-center items-center text-2xl text-blue-500">flowess</span>
-            <img
+            <span
+              class="not-italic text-left justify-center items-center text-2xl text-blue-500"
+              >flowess</span
+            >
+            <Icon
               class="absolute inline size-6 right-0 top-2"
-              src="path/to/icon5.png"
-              alt=""
+              icon-code="icon10"
             />
           </button>
         </div>
 
         <!-- sub-menu -->
         <div class="absolute top-14 left-4 w-4/5 border-b-2 border-gray-200">
-          <button @click="subone" class="flex justify-start size-4/6 mx-0 my-4">
-            <Icon class="inline size-7" icon-code="Demo"></Icon>
+          <button
+            @click="subone"
+            class="flex justify-start space-x-4 size-4/6 mx-0 my-4"
+          >
+            <Icon class="inline size-7" icon-code="icon1" />
             <span class="not-italic text-left text-l">EpanetDemo</span>
           </button>
           <div v-if="list1" class="mx-2 py-2 border-b bg-white">
-            <a class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Epanet Demo</a>
-            <a class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Mobile View</a>
-            <a class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Default View</a>
+            <a
+              class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white"
+              >Epanet Demo</a
+            >
+            <a
+              class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white"
+              >Mobile View</a
+            >
+            <a
+              class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white"
+              >Default View</a
+            >
           </div>
 
-          <button @click="subtwo" class="flex justify-start size-4/6 mx-0 my-4">
-            <Icon class="inline size-7" icon-code="Demo"></Icon>
+          <button
+            @click="subtwo"
+            class="flex justify-start space-x-4 size-4/6 mx-0 my-4"
+          >
+            <Icon class="inline size-7" icon-code="icon2" />
             <span class="not-italic text-left text-l">EpanetDemo</span>
           </button>
           <div v-if="list2" class="mx-2 py-2 border-b bg-white">
-            <a class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Epanet Demo</a>
-            <a class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Mobile View</a>
-            <a class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Default View</a>
+            <a
+              class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white"
+              >Epanet Demo</a
+            >
+            <a
+              class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white"
+              >Mobile View</a
+            >
+            <a
+              class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white"
+              >Default View</a
+            >
           </div>
-          
+
           <!-- Add more buttons as needed -->
+          <button class="flex justify-start space-x-4 size-4/6 mx-0 my-4">
+            <Icon class="inline" icon-code="icon3" />
+            <span class="not-italic text-left text-l">EpanetDemo</span>
+          </button>
+
+          <button class="flex justify-start space-x-4 size-4/6 mx-0 my-4">
+            <Icon class="inline" icon-code="icon4" />
+            <span class="not-italic text-left text-l">EpanetDemo</span>
+          </button>
+
+          <button class="flex justify-start space-x-4 size-4/6 mx-0 my-4">
+            <Icon class="inline" icon-code="icon5" />
+            <span class="not-italic text-left text-l">EpanetDemo</span>
+          </button>
+
+          <button class="flex justify-start space-x-4 size-4/6 mx-0 my-4">
+            <Icon class="inline" icon-code="icon6" />
+            <span class="not-italic text-left text-l">EpanetDemo</span>
+          </button>
+
+          <button class="flex justify-start space-x-4 size-4/6 mx-0 my-4">
+            <Icon class="inline" icon-code="icon7" />
+            <span class="not-italic text-left text-l">EpanetDemo</span>
+          </button>
+
+          <button class="flex justify-start space-x-4 size-4/6 mx-0 my-4">
+            <Icon class="inline" icon-code="icon8" />
+            <span class="not-italic text-left text-l">EpanetDemo</span>
+          </button>
         </div>
         <div class="absolute w-full bottom-2 left-4">
-          <span class="p-3 text-xl rounded-full bg-stone-200 text-blue-500">FR</span>
-          <img
-            class="inline size-7 m-6"
-            src="path/to/icon6.png"
-            alt=""
-          />
+          <span
+            class="absolute bottom-3 p-3 text-xl rounded-full bg-stone-200 text-blue-500"
+            >FR</span
+          >
+          <Icon class="absolute right-12 bottom-5 m-0" icon-code="icon10" />
         </div>
       </div>
     </div>
@@ -90,13 +137,13 @@
 </template>
 
 <script>
-import Icon from '@/components/Icon.vue';
+import Icon from "@/components/icon.vue";
 
 export default {
   data() {
     return {
       menu: false,
-      list1: false, 
+      list1: false,
       list2: false,
     };
   },
@@ -112,8 +159,8 @@ export default {
     },
   },
   components: {
-    Icon
-  }
+    Icon,
+  },
 };
 </script>
 

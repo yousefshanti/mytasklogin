cf344b2d85272acf4c7fdeb85ee5bc301a2c8a94
yousefshanti <yousefshanti22@gmail.com>
2024-08-24 20:17:43 +0300

    edit_on_de

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
-<template >
-  <body class="h-screen bg-stone-200">
-    <Submenu />
-    <Header></Header>
-  </body>
+<template>
+  <div class= "h-screen bg-stone-200">
+    <HeaderComponent title="Dashboard" />
+    <submenu class="top-9"/>
+   
+  </div>
 </template>
+    
+  
+
+
 
 <script>
 import Submenu from "./components/icons/sub-menu.vue";
 import { Icon } from "@iconify/vue";
 import IconWrapper from "./components/icons/icon-wrapper.vue";
-import Header from "./components/header.vue";
+import HeaderComponent from "./components/header.vue";
 export default {
   name: "App",
-  components: { Submenu,Header,IconWrapper,Icon },
+  components: { Submenu,HeaderComponent,IconWrapper,Icon },
 };
 </script>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,33 +1,40 @@
 <template>
+  <header class="w-full" >
+    <IconWrapper
+        class="text-blue-500 size-6 m-1 left-0  absolute inline"
+          icon-Code="icon-park-outline:waterfalls-v"
+        ></IconWrapper>
+    <span class="font-mono absolute left-9 top-1">{{ title }}</span>
     
       <IconWrapper
-        class="size-6 m-3 bg-white absolute top-5 right-12  "
+        class="size-5 m-1  bg-white absolute right-2 "
           icon-Code="zondicons:link"
         ></IconWrapper>
+
+   
         <IconWrapper
-        class="size-6 m-3 bg-white absolute top-5 right-4   "
+        class=" size-5  m-1 bg-white absolute  right-9 "
           icon-Code="teenyicons:pdf-outline"
         ></IconWrapper>
-  
-  </template>
-  
-  <script lang="ts">
-  import { Icon } from "@iconify/vue";
-  import IconWrapper from "./icons/icon-wrapper.vue";
-  export default {
-    name: 'Header',
-    props: {
-      title: {
-        type: String,
-        required: true
-      },
-      
-    },
-    components: {
-    Icon,
-    IconWrapper,
+
+  </header>
+</template>
+
+<script >
+import IconWrapper from "./icons/icon-wrapper.vue";
+export default {
+  components: {IconWrapper},
+  data(){
+    return{}
   },
-  };
-  </script>
-  
-  
+  name: "HeaderComponent",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+ 
+};
+</script>
+
